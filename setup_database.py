90e4b211eddb53df58d94533e31f5e2f542ffe69
James Gardiner <james@jgardiner.co.uk>
2018-05-22 16:25:52 +0100

    Move to a retry methodology

--- setup_database.py
+++ setup_database.py
@@ -1,18 +1,26 @@
 import os
 
 import sqlalchemy
+from retrying import retry
+
+def retry_if_sqlalchemy_error(exception):
+    print(f'error has occurred: {str(exception)}')
+    return isinstance(exception, sqlalchemy.exc.OperationalError)
+
+
+@retry(retry_on_exception=retry_if_sqlalchemy_error, wait_fixed=10000, stop_max_delay=600000, wrap_exception=True)
+def retry_connection(engine):
+    return engine.connect()
+
+
 
 if __name__ == '__main__':
     username = os.getenv('POSTGRES_USERNAME')
     password = os.getenv('POSTGRES_PASSWORD')
     port = os.getenv('EX_POSTGRES_PORT')
-    engine = sqlalchemy.create_engine(f'postgresql://{username}:{password}@localhost:{port}/postgres')
 
-    try:
-        conn = engine.connect()
-    except sqlalchemy.exc.OperationalError as e:
-        print('Cannot connect to postgres.')
-        exit 0
+    engine = sqlalchemy.create_engine(f'postgresql://{username}:{password}@localhost:{port}/postgres')
+    conn = retry_connection(engine)
 
     print('Dropping pgcrypto')
     conn.execute('DROP EXTENSION IF EXISTS pgcrypto;')
